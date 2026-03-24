e70a96349c81dad40a15e65a81c646e248d27750
Alain Sarti <alain.sarti@seibert.group>
2026-03-24 10:00:26 +0000

    feat(web-client): add sidebar and shared app layout

---
+++ sample-clients/data-web-client/src/components/app-layout.tsx
@@ -0,0 +1,10 @@
+import Sidebar from './sidebar';
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen bg-white">
+      <Sidebar />
+      <main className="flex-1 overflow-auto p-6">{children}</main>
+    </div>
+  );
+}
